77dd9ac176747c66b6f90d7097968037405d0a47
tung.nguyenvan <nguyentungtungtungnguyen@gmail.com>
2019-01-02 08:05:21 +0700

    - Update interface menu, and change use loop for

--- StudentInformation/Main.cpp
+++ StudentInformation/Main.cpp
@@ -6,6 +6,12 @@
 using namespace std;
 const string FILE_PATH = "Student_manager.txt";
 
+enum LoadFile
+{
+	LOADFAIL,
+	FILEEMPTY,
+	LOADSUCCESS
+};
 struct Student
 {
 	int _id;
@@ -23,7 +29,7 @@ void continueInsertStudent();
 bool checkIdExit(int id, vector<Student> students);
 bool insertStudent(Student student, vector<Student> *students);
 bool saveStudentsToFile(vector<Student> students);
-bool loadStudentsFromFile(vector<Student> *students);
+int loadStudentsFromFile(vector<Student> *students);
 
 int main() {
 	while (!wantExit)
@@ -35,17 +41,20 @@ int main() {
 	return 0;
 }
 
+/**
+* Show list question
+*/
 void question() {
 	system("cls");
-	cout << "------------ WELLCOME -----------" << endl << endl;
+	cout << "\t------------ WELLCOME -----------" << endl << endl;
 
-	cout << "1. Input" << endl;
-	cout << "2. Display" << endl;
-	cout << "3. Save to file" << endl;
-	cout << "4. Load from file" << endl;
-	cout << "0. Exit" << endl;
+	cout << "\t*\t1. Input\t\t*" << endl;
+	cout << "\t*\t2. Display\t\t*" << endl;
+	cout << "\t*\t3. Save to file\t\t*" << endl;
+	cout << "\t*\t4. Load from file\t*" << endl;
+	cout << "\t*\t0. Exit\t\t\t*" << endl;
 
-	cout << "----------------------------------" << endl << endl;
+	cout << "\t----------------------------------" << endl << endl;
 	cout << "Choose: ";
 
 	int select;
@@ -65,8 +74,20 @@ void question() {
 		system("pause");
 		break;
 	case 4:
-		if (loadStudentsFromFile(&students)) cout << "Load file to List success!" << endl;
-		else cout << "Load file to List Failed. Try it!" << endl;
+		switch (loadStudentsFromFile(&students))
+		{
+		case LOADFAIL: 
+			cout << "Load file to List Failed. Try it!" << endl;
+			break;
+		case FILEEMPTY:
+			cout << "File empty!" << endl;
+			break;
+		case LOADSUCCESS:
+			cout << "Load file to List success!" << endl;
+			break;
+		default:
+			break;
+		}
 		system("pause");
 		break;
 	case 0:
@@ -117,8 +138,8 @@ void getInforStudent() {
 * Check id not exist
 */
 bool checkIdExit(int id, vector<Student> students) {
-	for (int i = 0; i < students.size(); i++) {
-		if (students.at(i)._id == id)
+	for (Student student : students) {
+		if (student._id == id)
 			return false;
 	}
 	return true;
@@ -147,24 +168,30 @@ void continueInsertStudent() {
 	if (select == 'y') getInforStudent();
 }
 
+/**
+* Show list students to console
+*/
 void showStudents(vector<Student> students) {
 	system("cls");
 	cout << "ID\t\tFull Name\t\tScore" << endl;
-	for (int i = 0; i < students.size(); i++) {
-		cout << students.at(i)._id << "\t\t";
-		cout << students.at(i)._name << "\t\t";
-		cout << students.at(i)._score << endl;
+	for (Student student : students) {
+		cout << student._id << "\t\t";
+		cout << student._name << "\t\t";
+		cout << student._score << endl;
 	}
 	cout << endl << endl;
 	system("pause");
 }
 
+/**
+* Save list student to file
+*/
 bool saveStudentsToFile(vector<Student> students) {
 	ofstream insertToFile(FILE_PATH, std::ios::out);
-	for (int i = 0; i < students.size(); i++) {
-		insertToFile << students.at(i)._id << endl;
-		insertToFile << students.at(i)._name << endl;
-		insertToFile << students.at(i)._score << endl;
+	for (Student student : students) {
+		insertToFile << student._id << endl;
+		insertToFile << student._name << endl;
+		insertToFile << student._score << endl;
 		if (insertToFile.fail())
 			return false;
 	}
@@ -173,10 +200,13 @@ bool saveStudentsToFile(vector<Student> students) {
 	return true;
 }
 
-bool loadStudentsFromFile(vector<Student> *students) {
+/**
+* Load stdent from file to List
+*/
+int loadStudentsFromFile(vector<Student> *students) {
 	int oldSize = students->size();
 	ifstream fileToList(FILE_PATH);
-	if (fileToList.fail()) return false;
+	if (fileToList.fail()) return LOADFAIL;
 	int numLine = 0;
 	Student student;
 	string line;
@@ -206,6 +236,6 @@ bool loadStudentsFromFile(vector<Student> *students) {
 	}
 
 	fileToList.close();
-	if (oldSize < students->size()) return true;
-	else return false;
+	if (oldSize < students->size()) return LOADSUCCESS;
+	else return FILEEMPTY;
 }
