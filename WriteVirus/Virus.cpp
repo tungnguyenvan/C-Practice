3d3a4651659866e0df98cb9e5fd3a5b60edf26eb
Nguyen Van Tung <nguyentungtungtungnguyen@gmail.com>
2019-01-05 16:05:42 +0700

    - Implement LoadFileADN of Virus

--- WriteVirus/Virus.cpp
+++ WriteVirus/Virus.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include "Virus.h"
+#include <string>
+#include <fstream>
 
 using namespace std;
+
+const string FILE_PATH = "ATGX.bin";
+const int MAX_LENG = 255;
+int lengthADN = 0;
 
 Virus::Virus() {
 
@@ -16,7 +22,17 @@ Virus::Virus(const Virus &virus) {
 }
 
 void Virus::LoadADNInformation() {
+	string adn;
+	ifstream file(FILE_PATH, ios::in);
+	if (file.fail()) {
+		cout << "cannot open file" << endl;
+		return;
+	}
 
+	if (!file.eof()) {
+		file >> adn;
+	}
+	this->m_dna = const_cast<char*>(adn.c_str());
 }
 
 void Virus::ReduceResistance(int medicine_resistance) {
