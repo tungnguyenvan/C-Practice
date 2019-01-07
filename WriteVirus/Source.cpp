da56257023c5d90e758363dad389e2d0ca7007e2
Nguyen Van Tung <nguyentungtungtungnguyen@gmail.com>
2019-01-07 19:11:20 +0700

    + Update patient alive

--- WriteVirus/Source.cpp
+++ WriteVirus/Source.cpp
@@ -10,19 +10,25 @@ int main() {
 	while (p.GetState() == 1)
 	{
 		system("cls");
-		cout << "\n\ntotal virus: " << p.GetListVirus().size() << endl;
-		cout << "Resistance: [Patient: " << p.GetResistance() << "]\t[Virus: " << p.GetTotalResistanceVirus() << "]" << endl << endl;
-		printf("Take Medicine (0 = NO, 1 = YES): ");
-		cin >> t; cout << endl;
-		if (t == '1') {
-			int min = 1;
-			int max = 60;
-			int medicine_resistance = min + (rand() % (int)(max - min + 1));
-			cout << medicine_resistance << endl;
-			p.TakeMadicine(medicine_resistance);
+		if (p.GetListVirus().size() > 0) {
+			cout << "\n\ntotal virus: " << p.GetListVirus().size() << endl;
+			cout << "Resistance: [Patient: " << p.GetResistance() << "]\t[Virus: " << p.GetTotalResistanceVirus() << "]" << endl << endl;
+			printf("Take Medicine (0 = NO, 1 = YES): ");
+			cin >> t; cout << endl;
+			if (t == '1') {
+				int min = 1;
+				int max = 60;
+				int medicine_resistance = min + (rand() % (int)(max - min + 1));
+				cout << medicine_resistance << endl;
+				p.TakeMadicine(medicine_resistance);
+			}
+			else if (t == '0') {
+				p.TakeMadicine(0);
+			}
 		}
-		else if (t == '0') {
-			p.TakeMadicine(0);
+		else {
+			cout << "Patient Alive..." << endl;
+			break;
 		}
 	}
 
